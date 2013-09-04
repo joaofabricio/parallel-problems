a7ac773425d8c0dd5aa9571b4bb309ae01eebfa9
João Fabrício Filho <jfabri2@gmail.com>
2013-09-04 13:49:59 -0300

    Multiplicação de matrizes (seq)

--- mm.c
+++ mm.c
@@ -8,19 +8,13 @@ int** mm(int** m1, int** m2, int m, int n) {
 
 	for (i=0; i<m; i++) {
 		for (j=0; j<n; j++) {
-			res[i][j] = soma(m1, m2, i, j, m, n);	
+			int k;
+			for (k=0; k<m; k++) {
+				res[i][j] += m1[i][k]*m2[k][j];
+			}
 		}
 	}
 	return res;
 	
 }
 
-int soma(int** m1, int** m2, int i, int j, int m, int n) {
-	int k, l, soma = 0;
-	for (k=0; k<m; k++) {
-		for (l=0; l<n; l++) {
-			soma += m1[i][k]*m2[j][l];
-		}
-	}
-	return soma;
-}
--- tests/mm.c
+++ tests/mm.c
@@ -3,23 +3,46 @@
 #include "../mm.c"
 
 void mm_test() {
-	int *m1[2], *m2[2], *exp[2];
-	int i, j;
-	for (i=0; i<2; i++) {
-		m1[i] = malloc(2*sizeof(int));
-		m2[i] = malloc(2*sizeof(int));
-		exp[i] = malloc(2*sizeof(int));
-		for (j=0; j<2; j++) {
-			m1[i][j] = m2 [i][j] = 1;
-			exp[i][j] = 2;
-		}
-	}
+	int m = 2, n = 4;
+	int **m1, **m2, **e;
 
-	int** m = mm(m1, m2, 2, 2);
+	m1 = malloc(2 * sizeof(int*));
+	m1[0] = malloc(m * sizeof(int));
+	m1[1] = malloc(m * sizeof(int));
+	m1[0][0] = 5;
+	m1[0][1] = 2;
+	m1[1][0] = 3;
+	m1[1][1] = 2;
 
-	for (i=0; i<2; i++) {
+	m2 = malloc(m * sizeof(int*));
+	m2[0] = malloc(n * sizeof(int));
+	m2[1] = malloc(n * sizeof(int));
+	m2[0][0] = 0;
+	m2[0][1] = 5;
+	m2[0][2] = 1;
+	m2[0][3] = 6;
+	m2[1][0] = 2;
+	m2[1][1] = -1;
+	m2[1][2] = 4;
+	m2[1][3] = -3;
+
+	e = malloc(m * sizeof(int*));
+	e[0] = malloc(n * sizeof(int));
+	e[1] = malloc(n * sizeof(int));
+	e[0][0] = 4;
+	e[0][1] = 23;
+	e[0][2] = 13;
+	e[0][3] = 24;
+	e[1][0] = 4;
+	e[1][1] = 13;
+	e[1][2] = 11;
+	e[1][3] = 12;
+
+	int** mat = mm(m1, m2, m, n);
+	int i,j;
+	for (i=0; i<m; i++) {
 		for (j=0; j<2; j++) {
-			assert(m[i][j] == exp[i][j]);
+			assert(mat[i][j] == e[i][j]);
 		}
 	}
 }
