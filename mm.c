dcf3c7952d8d98dccfad8dcbb80cbbadb5413ac7
João Fabrício Filho <jfabri2@gmail.com>
2013-09-05 12:47:13 -0300

    Multiplicação de matrizes (seq) - correção

--- mm.c
+++ mm.c
@@ -2,11 +2,12 @@
 
 int** mm(int** m1, int** m2, int m, int n) {
 
-	int i, j;
+	int i;
 
-	int** res = matrix_instance(m, n);	
+	int** res = matrix_instance(m, n, 0);
 
 	for (i=0; i<m; i++) {
+		int j;
 		for (j=0; j<n; j++) {
 			int k;
 			for (k=0; k<m; k++) {
--- tests/mm.c
+++ tests/mm.c
@@ -1,53 +1,32 @@
 #include <assert.h>
+#include <time.h>
 
 #include "../mm.c"
 
-void mm_test() {
-	int m = 2, n = 4;
-	int **m1, **m2, **e;
+void mm_exec(int m, int n, int p) {
 
-	m1 = malloc(2 * sizeof(int*));
-	m1[0] = malloc(m * sizeof(int));
-	m1[1] = malloc(m * sizeof(int));
-	m1[0][0] = 5;
-	m1[0][1] = 2;
-	m1[1][0] = 3;
-	m1[1][1] = 2;
+        int **m1 = matrix_instance(p, m, 1);
 
-	m2 = malloc(m * sizeof(int*));
-	m2[0] = malloc(n * sizeof(int));
-	m2[1] = malloc(n * sizeof(int));
-	m2[0][0] = 0;
-	m2[0][1] = 5;
-	m2[0][2] = 1;
-	m2[0][3] = 6;
-	m2[1][0] = 2;
-	m2[1][1] = -1;
-	m2[1][2] = 4;
-	m2[1][3] = -3;
+	int **m2 = matrix_instance(m, n, 1);
+	
+	clock_t start = clock();
+	int **r = mm(m1, m2, m, n);
+	clock_t end = clock();
+	float seconds = (float) end - start / CLOCKS_PER_SEC;
+	printf("%f\n", seconds);
 
-	e = malloc(m * sizeof(int*));
-	e[0] = malloc(n * sizeof(int));
-	e[1] = malloc(n * sizeof(int));
-	e[0][0] = 4;
-	e[0][1] = 23;
-	e[0][2] = 13;
-	e[0][3] = 24;
-	e[1][0] = 4;
-	e[1][1] = 13;
-	e[1][2] = 11;
-	e[1][3] = 12;
-
-	int** mat = mm(m1, m2, m, n);
 	int i,j;
-	for (i=0; i<m; i++) {
-		for (j=0; j<2; j++) {
-			assert(mat[i][j] == e[i][j]);
-		}
-	}
+        for (i=0; i<m; i++) {
+                for (j=0; j<n; j++) {
+                        assert(r[i][j] == n);
+                }
+        }
+	
 }
 
 int main() {
-	mm_test();
+	mm_exec(1000, 1000, 1000);
 	return 0;
 }
+
+
