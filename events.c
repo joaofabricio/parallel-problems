15b5447abc94d0fdeea2c3493d67c855e1fbf946
João Fabrício Filho <jfabri2@gmail.com>
2013-10-02 22:25:19 -0300

    Broadcast

--- events.c
+++ events.c
@@ -3,64 +3,69 @@
 
 #include "utils.c"
 
-#define WORK_SIZE 200
+#define WORK_SIZE 1000
 
 typedef struct work_t {
-	int** value;
-	clock_t time;
+	int value;
+	int timestamp;
 	void *next;
 } work_t;
 
-work_t *w;
+work_t *list;
+
+work_t message;
 
 pthread_mutex_t lock;
 
 int manager_seq = 0;
 
+int get_value() {
+	return 1111;
+}
+
+int has_work() {
+	return 1;
+}
+
+void send(work_t w) {
+	pthread_mutex_lock(&lock);
+	w.next = (void*) list;
+	list = &w;
+	message = w;
+	pthread_mutex_unlock(&lock);
+	printf("send %d\n", w.timestamp);
+}
+
+int max(int a, int b) {
+	return a>b? a : b;
+}
+
 void work(void* arg) {
-	int** mm(int** m1, int** m2, int m, int n) {
+	int rx = 0;
+	while (has_work()) {
 
-		int i;
+		//receive
+		rx = max(rx, message.timestamp);
+		rx++;
+		
+		work_t temp;
+		temp.value = get_value();
 
-		int** res = matrix_instance(m, n, 0);
-
-		for (i=0; i<m; i++) {
-			int j;
-			for (j=0; j<n; j++) {
-				int k;
-				for (k=0; k<m; k++) {
-					res[i][j] += m1[i][k]*m2[k][j];
-				}
-			}
-		}
-		return res;
+		temp.timestamp = rx;
+		send(temp);
+		rx++;
 
 	}
-
-	work_t temp;
-	temp.value = mm(matrix_instance(WORK_SIZE, WORK_SIZE, 0), 
-			matrix_instance(WORK_SIZE, WORK_SIZE, 0),
-			WORK_SIZE,
-			WORK_SIZE);
-	temp.time = clock();
-
-	pthread_mutex_lock(&lock);
-	temp.next = (void*) w;
-	w = &temp;
-	pthread_mutex_unlock(&lock);
-
 	pthread_exit(NULL);
 }
 
 void start_threads(int nthreads) {
-	w = NULL;
+	list = NULL;
 	int i;
 	pthread_t threads[nthreads];
-	//threads = malloc(nthreads * sizeof(pthread_t));
 	for (i=0; i<nthreads; i++) {
 		pthread_create(&threads[i], NULL, (void *) &work, NULL);
 	}
 	for (i=0; i<nthreads; i++)
 		pthread_join(threads[i], NULL);
 }
-
