e64256105c7970959c4e577fc9dd261d39de3c1d
João Fabrício Filho <jfabri2@gmail.com>
2013-10-04 21:17:05 -0300

    events corrections

--- events.c
+++ events.c
@@ -11,11 +11,17 @@ typedef struct work_t {
 	void *next;
 } work_t;
 
+typedef struct message_t {
+	int timestamp, new;
+} message_t;
+
 work_t *list;
 
-work_t message;
+message_t message;
 
 pthread_mutex_t lock;
+
+pthread_cond_t cond;
 
 int manager_seq = 0;
 
@@ -31,22 +37,26 @@ void send(work_t w) {
 	pthread_mutex_lock(&lock);
 	w.next = (void*) list;
 	list = &w;
-	message = w;
+	if (w.timestamp > message.timestamp)
+		message.timestamp = w.timestamp;
+	message.new = 1;
 	pthread_mutex_unlock(&lock);
 	printf("send %d\n", w.timestamp);
+	pthread_cond_broadcast(&cond);
 }
 
 int max(int a, int b) {
 	return a>b? a : b;
 }
 
+int has_message() {
+	return message.new;
+}
+
 void work(void* arg) {
 	int rx = 0;
+	message.new = 0;
 	while (has_work()) {
-
-		//receive
-		rx = max(rx, message.timestamp);
-		rx++;
 		
 		work_t temp;
 		temp.value = get_value();
@@ -55,6 +65,14 @@ void work(void* arg) {
 		send(temp);
 		rx++;
 
+		//receive
+		pthread_mutex_lock(&lock);
+		if (!has_message()) 
+			pthread_cond_wait(&cond, &lock);
+		rx = max(rx, message.timestamp);
+		message.new = 0;
+		pthread_mutex_unlock(&lock);
+		rx++;
 	}
 	pthread_exit(NULL);
 }
@@ -64,7 +82,7 @@ void start_threads(int nthreads) {
 	int i;
 	pthread_t threads[nthreads];
 	for (i=0; i<nthreads; i++) {
-		pthread_create(&threads[i], NULL, (void *) &work, NULL);
+		pthread_create(&threads[i], NULL, (void *) &work, (void *) &i);
 	}
 	for (i=0; i<nthreads; i++)
 		pthread_join(threads[i], NULL);
