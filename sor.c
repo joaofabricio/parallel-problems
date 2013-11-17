022d3da4d41034dc5c2cea5e67a4453833269319
João Fabrício Filho <jfabri2@gmail.com>
2013-11-17 12:48:06 -0200

    SOR - versao inicial

--- sor.c
+++ sor.c
@@ -1,12 +1,59 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
-int** sor(int** m, int l, int c, double fator) {
+#define NTHREADS 5
+
+int lines, columns;
+
+int** m;
+
+pthread_barrier_t barrier;
+
+typedef struct work_t {
+	int** m;
+	int** ret;
+	int lines, columns, id;
+} work_t;
+
+void worker(void *w) {
+
+	work_t *work = (work_t*) w;
+
+	int id = work->id;
+
+	#ifdef DEBUG
+	printf("initializing thread %d\n", id);
+	#endif
+
+	int begin_col, end_col;
+	int begin_lin, end_lin;
+
+	begin_lin = work->lines/NTHREADS * id;
+	end_lin = work->lines/NTHREADS * (id+1);
+	begin_col = work->columns/NTHREADS * id;
+	end_col = work->columns/NTHREADS * (id+1);
+
+	int i, j;
+	for (i=begin_lin+1; i<end_lin-1; i++) {
+		for (j=begin_col+1; j<end_col-1; j++) {
+			int up = work->m[i-1][j];
+			int down = work->m[i+1][j];
+			int left = work->m[i][j-1];
+			int right = work->m[i][j+1];
+
+			work->ret[i][j] = (up+down+left+right)/4;
+		}
+	}
+	pthread_barrier_wait(&barrier);
+}
+
+int** sor(int** m, int l, int c, int n) {
 
 	int** ret = malloc(l * sizeof(int));
 
-	int i, j;
-	
+	int i, j, k;
+
 	for (i=0; i<l; i++) {
 		ret[i] = malloc(c * sizeof(int));
 		for (j=0; j<c; j++) {
@@ -14,16 +61,26 @@ int** sor(int** m, int l, int c, double fator) {
 		}
 	}
 
-	for (i=1; i<l-1; i++) {
-		for (j=1; j<c-1; j++) {
-			int up = m[i-1][j];
-			int down = m[i+1][j];
-			int left = m[i][j-1];
-			int right = m[i][j+1];
+	pthread_t threads[NTHREADS];
 
-			ret[i][j] = (up+down+left+right)/4*fator;
+	for (i=0; i<n; i++) {
+		pthread_barrier_init(&barrier, NULL, NTHREADS);
+		for (k=0; k<NTHREADS; k++) {
+			work_t *work = malloc(sizeof(work_t));
+			work->id = k;
+			work->lines = l;
+			work->columns = c;
+			work->m = m;
+			work->ret = ret;
+			pthread_create(&threads[k], NULL, (void*) worker, (void*) work);
 		}
+		for (k=0; k<NTHREADS; k++)
+			pthread_join(threads[k], NULL);
+		pthread_barrier_destroy(&barrier);
 	}
+
+
 
 	return ret;
 }
+
--- tests/sor.c
+++ tests/sor.c
@@ -1,28 +1,18 @@
 #include <assert.h>
 
 #include "../sor.c"
+#include "../utils.c"
 
 void sor_test() {
-	int* m[3];// = {{1,1,1}, {1,1,1}, {1,1,1}};
-	int i, j;
-	
-	for(i=0; i<3; i++) {
-		m[i] = malloc(sizeof(int)*3);
-		for (j=0;j<3;j++)
-			m[i][j] =1;
-	}
 
-	int expected[3][3];// = {{1,1,1}, {1,2,1},{1,1,1}};
-	for(i=0; i<3; i++) 
-                for (j=0;j<3;j++)
-                        expected[i][j] =1;
-	expected[1][1] = 2;
+	int** m = matrix_instance(50, 50, 1);
 
-	int** ret = sor(m, 3, 3, 2);
+	int** ret = sor(m, 50, 50, 5);
 
+	int i,j;
 	for(i=0; i<3; i++) {
 		for (j=0; j<3; j++) {
-			assert(ret[i][j] == expected[i][j]);
+			assert(ret[i][j] == 1);
 		}
 	}
 }
